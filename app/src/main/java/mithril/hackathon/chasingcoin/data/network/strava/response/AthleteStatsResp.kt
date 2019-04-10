340e2e244af93948c7e4a643f7b073073ede77dd
kennethchiao <chiao.kenneth@gmail.com>
2019-04-10 11:03:33 +0800

    modify AthleteStats json format

--- app/src/main/java/mithril/hackathon/chasingcoin/data/network/strava/response/AthleteStatsResp.kt
+++ app/src/main/java/mithril/hackathon/chasingcoin/data/network/strava/response/AthleteStatsResp.kt
@@ -1,19 +1,33 @@
 package mithril.hackathon.chasingcoin.data.network.strava.response
 
 import com.google.gson.annotations.SerializedName
-import java.util.*
 
 data class AthleteStatsResp(
-    @SerializedName("country") val country: String,
-    @SerializedName("running_race_type") val runningRaceType: Int,
-    @SerializedName("route_ids") val routeIds: IntArray,
-    @SerializedName("distance") val distance: Float,
-    @SerializedName("city") val city: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("id") val id: Int,
-    @SerializedName("state") val state: String,
-    @SerializedName("url") val url: String,
-    @SerializedName("start_date_local") val date: Date
-
-)
+    @SerializedName("recent_run_totals") val recentRunTotals: Int?,
+    @SerializedName("all_run_totals") val allRunTotals: Int?,
+    @SerializedName("recent_swim_totals") val recentSwimTotals: Int?,
+    @SerializedName("biggest_ride_distance") val biggestRideDistance: Float,
+    @SerializedName("ytd_swim_totals") val ytdSwimTotals: Int?,
+    @SerializedName("all_swim_totals") val allSwimTotals: Int?,
+    @SerializedName("recent_ride_totals") val recentRideTotals: RecentRideTotals,
+    @SerializedName("biggest_climb_elevation_gain") val biggestClimbElevationGain: Float,
+    @SerializedName("ytd_ride_totals") val ytdRideTotals: Int?,
+    @SerializedName("all_ride_totals") val allRideTotals:Int?,
+    @SerializedName("ytd_run_totals") val ytdRunTotals:Int?
+){
+    data class RecentRideTotals(
+        @SerializedName("distance")
+        val distance: Float?,
+        @SerializedName("achievement_count")
+        val achievementCount: Int,
+        @SerializedName("count")
+        val count: Int,
+        @SerializedName("elapsed_time")
+        val elapsedTime: Int,
+        @SerializedName("elevation_gain")
+        val elevationGain: Float?,
+        @SerializedName("moving_time")
+        val movingTime: Int
+    )
+}
 
